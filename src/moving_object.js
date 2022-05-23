32dd7e6b1d604e7e91a5a78f548f6cd4dd8ae170
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-23 14:24:00 -0400

    finished inheritance

--- src/moving_object.js
+++ src/moving_object.js
@@ -8,8 +8,12 @@ function MovingObject(options) {
 MovingObject.prototype.draw = function (ctx) {
     ctx.fillStyle = this.color
     ctx.beginPath();
-    ctx.arc(this.position[0], this.position[1], this.radius, 0, 2 * Math.PI, true);
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true);
     ctx.fill()
+}
+
+MovingObject.prototype.move = function() {
+    this.pos = [this.pos[0] + this.vel[0], this.pos[1] + this.vel[1]]
 }
 
 module.exports = MovingObject;
